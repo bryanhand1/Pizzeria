8e10d6359b48aaa4a7e5fb4eca683c3f764f2663
Bryan Hand <bryan_hand1@baylor.edu>
2021-04-26 13:31:34 -0500

    Added Models and Entries

--- pizzas/admin.py
+++ pizzas/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+
 # Register your models here.
+from .models import Pizza
+
+admin.site.register(Pizza)
+
+from .models import Topping
+
+admin.site.register(Topping)
