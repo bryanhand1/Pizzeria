36cf9d5001b852c71bbd3b7f6cc2fdbe0290ae34
Bryan Hand <bryan_hand1@baylor.edu>
2021-04-29 15:06:00 -0500

    Final Substance/Add Photos

--- pizzas/models.py
+++ pizzas/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Pizza(models.Model):
     name = models.CharField(max_length = 200)
+    photo = models.ImageField(upload_to='images/', null=True)
 
     def __str__(self):
         return self.name
--- pizzas/urls.py
+++ pizzas/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from django.conf import settings # new
+from django.urls import path, include # new
+from django.conf.urls.static import static # new
 
 from . import views
 
@@ -12,3 +15,6 @@ urlpatterns = [
 
 
 ]
+
+if settings.DEBUG: # new
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
