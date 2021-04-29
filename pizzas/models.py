8165f1515535a7f3ec5526ae9cb19eb1894d0d65
Bryan Hand <bryan_hand1@baylor.edu>
2021-04-29 13:00:23 -0500

    Add Comments to display on Pizza Site

--- pizzas/models.py
+++ pizzas/models.py
@@ -11,5 +11,14 @@ class Topping(models.Model):
     pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
     name = models.CharField(max_length = 200)
 
+
     def __str__(self):
         return self.name
+
+class Comment(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
