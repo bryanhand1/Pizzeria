a8cfc3db99bc1739ea71ba8f3df2d6686c4f3482
Bryan Hand <bryan_hand1@baylor.edu>
2021-04-29 14:43:10 -0500

    Add comments

--- pizzas/views.py
+++ pizzas/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Pizza, Topping
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -24,6 +25,27 @@ def pizza(request, pizza_id):
     context = {'pizza':pizza, 'toppings':toppings, 'comments':comments}
 
     return render(request, 'pizzas/pizza.html', context)
+
+def new_comment(request, pizza_id):
+
+    pizza = Pizza.objects.get(id=pizza_id)
+    if request.method != 'POST':
+        form = CommentForm()
+    else:
+        form = CommentForm(data=request.POST)
+
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.pizza = pizza
+            new_comment.save()
+            form.save()
+            return redirect('pizzas:pizza', pizza_id=pizza_id)
+
+    context = {'form':form, 'pizza':pizza}
+    return render(request, 'pizzas/new_comment.html', context)
+
+
+
 
 
 
